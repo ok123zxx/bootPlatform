6cb7f04cc91c4b226b0b4ff8fa47583ee55b6271
zengjian <zengjian@winhong.com>
2018-04-08 17:45:03 +0800

    修改部署脚本

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ..
 git pull
 mvn clean install
 ps -ef | grep myapp-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | while read pid
@@ -7,5 +8,7 @@ do
     kill -9 $pid
     echo "kill result: $?"
 done
-cd target
-nohup java -jar myapp* >> out.log 2>&1 &
+cd bootSchedule/target
+nohup java -jar boot* >> boot.log 2>&1 &
+cd myApp/target
+nohup java -jar myapp* >> app.log 2>&1 &
