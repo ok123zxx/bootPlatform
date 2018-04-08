5ee62712fb9f4b46ab1163aa6056d44af4535b44
zengjian <zengjian@winhong.com>
2018-04-08 21:20:18 +0800

    修改部署脚本

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -2,12 +2,23 @@
 cd ..
 git pull
 mvn clean install
+
+
+ps -ef | grep boot-schedule-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | while read pid
+do
+    echo "boot-schedule is running, to kill pid=$pid"
+    kill -9 $pid
+    echo "kill result: $?"
+done
+
 ps -ef | grep myapp-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | while read pid
 do
     echo "myapp is running, to kill pid=$pid"
     kill -9 $pid
     echo "kill result: $?"
 done
+
+
 cd bootSchedule/target
 nohup java -jar boot-schedule-1.0-SNAPSHOT.jar >> boot.log 2>&1 &
 cd ..
