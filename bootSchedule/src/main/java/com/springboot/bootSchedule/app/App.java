9273e3d0e57f31be63e85dacfa877d5d0355abb9
zengjian <zengjian@winhong.com>
2018-04-08 13:57:48 +0800

    增加schedule的配置

--- bootSchedule/src/main/java/com/springboot/bootSchedule/app/App.java
+++ bootSchedule/src/main/java/com/springboot/bootSchedule/app/App.java
@@ -1,15 +1,16 @@
-//package com.springboot.bootSchedule.app;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.ComponentScan;
-//
-//
-//@SpringBootApplication
-//@ComponentScan(basePackages = {
-//		"com.springboot.bootSchedule"})
-//public class App {
-//	public static void main(String[] args) {
-//		SpringApplication.run(App.class, args);
-//	}
-//}
+package com.springboot.bootSchedule.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.springboot.bootSchedule",
+        "com.springboot.base"})
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
