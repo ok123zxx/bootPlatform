6ac7bb7a8bd539f82f57683e852b4832773e12bd
zengjian <zengjian@winhong.com>
2018-05-03 22:49:46 +0800

    添加了eureka，但是没有跑起来

---
+++ eureka-app/src/main/java/com/springboot/caion/eureka/EurekaApp.java
@@ -0,0 +1,18 @@
+package com.springboot.caion.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by zengJian on 2018/5/3<br>
+ * <br>
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class, args);
+    }
+}
