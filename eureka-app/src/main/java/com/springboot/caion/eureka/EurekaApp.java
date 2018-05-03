f4b4057eb012d2e4abeb80477ac354ad75493768
zengjian <zengjian@winhong.com>
2018-05-03 23:01:18 +0800

    测试eureka多实例启动

--- eureka-app/src/main/java/com/springboot/caion/eureka/EurekaApp.java
+++ eureka-app/src/main/java/com/springboot/caion/eureka/EurekaApp.java
@@ -10,10 +10,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * Created by zengJian on 2018/5/3<br>
  * <br>
  */
-
 @EnableEurekaServer
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class EurekaApp {
+    //java -jar .\eureka-app-1.0-SNAPSHOT.jar --server.port=8001
+    //java -jar .\eureka-app-1.0-SNAPSHOT.jar --server.port=8000
     public static void main(String[] args) {
         SpringApplication.run(EurekaApp.class, args);
     }
