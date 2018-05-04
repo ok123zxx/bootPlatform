8dea09829ff55a0955a923c8ac097e645b0492a0
zengjian <zengjian@winhong.com>
2018-05-04 14:02:24 +0800

    开发环境，eureka单实例，服务器eureka多实例启动

---
+++ eureka-app/src/main/resources/deploy.bat
@@ -0,0 +1,3 @@
+cd H:\project\bootPlatform\eureka-app\target
+java -jar eureka-app-1.0-SNAPSHOT.jar --spring.profiles.active=peer1
+java -jar eureka-app-1.0-SNAPSHOT.jar --spring.profiles.active=peer2
