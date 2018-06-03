6e631026eb42c143fd6eadb99323f1e5485883b8
zengjian <zengjian@winhong.com>
2018-06-03 22:23:45 +0800

    加入爬虫测试代码

--- boot-service/src/main/java/com/springboot/myapp/service/PaChongService.java
+++ boot-service/src/main/java/com/springboot/myapp/service/PaChongService.java
@@ -9,13 +9,11 @@ import org.apache.commons.collections.CollectionUtils;
 import org.assertj.core.util.Lists;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
